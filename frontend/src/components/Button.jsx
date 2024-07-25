9464f9f5d41e6ba25961e336c158e6f7b6b7c445
Rohan-Munot <rohanmunot24@gmail.com>
2024-07-25 11:03:26 +0530

    aa

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line react/prop-types
 export function Button({label, onClick}) {
     return (
-        <button onClick={onClick} type={"button"} className={'w-full text-white bg-gray-800 font-medium py-2.5 px-5 me-2 mb-2'}>
-            {label}
-        </button>
+        <div >
+            <button onClick={onClick} type={"button"} className={'w-full text-white bg-gray-800 font-medium py-2.5 px-5 me-2 mb-2'}>
+                {label}
+            </button>
+        </div>
     )
 }
